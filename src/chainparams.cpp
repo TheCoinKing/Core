8b5a26c67a88dbb75805fa0045a14868a03a0a77
TheCoinKing <34919477+TheCoinKing@users.noreply.github.com>
2018-01-03 23:56:08 +0100

    Fix?

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -71,10 +71,10 @@ public:
         if (genesis.GetHash() != hashGenesisBlock)
         {
             Logprintf("recalculating params for mainnet.\n");
-            Logprintf("old mainnet genesis nonce: %s\n", genesis.nNonce.ToString().c_str());
+            Logprintf("old mainnet genesis nonce: %u\n", genesis.nNonce);
             Logprintf("old mainnet genesis hash:  %s\n", hashGenesisBlock.ToString().c_str());
             // deliberately empty for loop finds nonce value.
-            for(genesis.nNonce == 0; genesis.GetHash() > bnProofOfWorkLimit; genesis.nNonce++){ } 
+            for(genesis.nNonce == 0; CBigNum(genesis.GetHash()) > bnProofOfWorkLimit; genesis.nNonce++){ } 
             Logprintf("new mainnet genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
             Logprintf("new mainnet genesis nonce: %s\n", genesis.nNonce.ToString().c_str());
             Logprintf("new mainnet genesis hash: %s\n", genesis.GetHash().ToString().c_str());
